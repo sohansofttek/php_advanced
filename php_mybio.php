867abcc6feba3168b5a146592a9a4df847c4767e
sohansofttek <1720123.sohanurrahman@gmail.com>
2023-04-12 02:30:16 +0600

    my bio

---
+++ php_mybio.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "MD SOHANUR RAHMAN";
+        echo "<br/>";
+        echo "my contact number 01744503744";
+        echo "<br/>";
+        echo "HOMETOWN:Dinajpur";
+    
+    ?>
+    
+</body>
+</html>
